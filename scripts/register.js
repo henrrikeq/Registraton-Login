783093efb459ae00e40b43c4141362d47d3f2f89
Henrrike Quarshie <henrrikequarshie@gmail.com>
2024-09-17 15:52:19 +0000

    added registration success

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,23 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector('#register'); 
+console.log(registerForm);
 // Handle form submit event
-registerForm.onsubmit = function(event) {
-    event. preventDefault();
-console.log(event);
+registerForm.addEventListener('submit', function (event) {
+    event.preventDefault(); 
+    // collect form data
+    const formData = new FormData(registerForm);
+    // send data to backend
+    // display success message to user
+    const messageH1 = document.querySelector('#message');
+    messageH1.textContent = 'Account registered successfully!';
+});
+
+// declare variable age
+let age = 71;
+// define squareAgeFunction
+function squareAge (age){
+    return age ** 2;
 }
+
+const squaredAge = squareAge(34);
+console.log(squaredAge);
